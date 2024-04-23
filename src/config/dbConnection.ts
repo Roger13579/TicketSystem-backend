b239030d3faefe3a416efff092c2a0a899aa233b
RogerLi <mingyao135@gmail.com>
2024-04-23 22:22:17 +0800

    feat: adjust db connection docs: add dockerfile

--- src/config/dbConnection.ts
+++ src/config/dbConnection.ts
@@ -2,12 +2,12 @@ import { connect } from 'mongoose';
 import env from 'dotenv';
 import log4js from './log4js';
 const logger = log4js.getLogger(`initConnection`);
-env.config();
 
 const connection = async () => {
+  env.config();
   try {
-    const mongoDbUrl =
-      process.env.MONGO_DB_URL || 'mongodb://localhost:27017/test';
+    const mongoDbUrl = process.env.MONGO_DB_URL || 'mongodb://localhost:27017';
+    logger.info(mongoDbUrl);
     await connect(mongoDbUrl);
     logger.info('mongo db connection initialized');
   } catch (err) {
