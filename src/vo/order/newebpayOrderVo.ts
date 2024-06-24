60950a1cc08c3f77300cdc75c8427bc087c7b119
RogerLi <mingyao135@gmail.com>
2024-06-24 20:09:16 +0800

    fix: modify vo fields name

--- src/vo/order/newebpayOrderVo.ts
+++ src/vo/order/newebpayOrderVo.ts
@@ -1,17 +1,17 @@
 export class NewebpayOrderVo {
   private readonly orderId: string;
-  private readonly merchantID: string;
-  private readonly tradeSha: string;
-  private readonly tradeInfo: string;
-  private readonly version: string;
+  private readonly MerchantID: string;
+  private readonly TradeSha: string;
+  private readonly TradeInfo: string;
+  private readonly Version: string;
   private readonly paymentGateway: string;
 
   constructor(orderId: string, tradeSha: string, tradeInfo: string) {
     this.orderId = orderId;
-    this.merchantID = process.env.NEWEBPAY_MERCHANT_ID;
-    this.tradeSha = tradeSha;
-    this.tradeInfo = tradeInfo;
-    this.version = process.env.NEWEBPAY_VERSION;
+    this.MerchantID = process.env.NEWEBPAY_MERCHANT_ID;
+    this.TradeSha = tradeSha;
+    this.TradeInfo = tradeInfo;
+    this.Version = process.env.NEWEBPAY_VERSION;
     this.paymentGateway = process.env.NEWEBPAY_PAY_GATEWAY;
   }
 }
