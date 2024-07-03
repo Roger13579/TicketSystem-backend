072f4fbcffb41d22692c47b88213b078e9c3474e
RogerLi <mingyao135@gmail.com>
2024-07-03 22:52:27 +0800

    fix: add newebpay order product name,

--- src/dto/order/newebpayOrderDto.ts
+++ src/dto/order/newebpayOrderDto.ts
@@ -16,15 +16,22 @@ export class NewebpayOrderDto {
   get email(): string {
     return this._email;
   }
+  get productName(): string {
+    return this._productName;
+  }
   private readonly _TimeStamp: number;
   private readonly _Amt: number;
   private readonly _MerchantOrderNo: string;
   private readonly _email: string;
+  private readonly _productName: string;
 
   constructor(order: IOrder) {
     this._TimeStamp = Math.round(moment().toDate().getTime() / 1000);
     this._Amt = order.price;
     this._MerchantOrderNo = order.id.toString();
+    this._productName = order.products.reduce((accumulator, product) => {
+      return accumulator ? `${accumulator}, ${product.title}` : product.title;
+    }, '');
     this._email =
       order.deliveryInfo == undefined
         ? 'test@gmail.com'
